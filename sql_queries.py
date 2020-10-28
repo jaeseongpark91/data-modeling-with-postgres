96e9625b16a8fb28346f546195dca29157f0d12c
jaeseongpark <46639995+jaeseongpark91@users.noreply.github.com>
2020-10-28 11:45:30 -0400

    Update sql_queries.py

--- sql_queries.py
+++ sql_queries.py
@@ -4,12 +4,12 @@ songplay_table_drop = "DROP TABLE IF EXISTS songplays"
 user_table_drop = "DROP TABLE IF EXISTS users"
 song_table_drop = "DROP TABLE IF EXISTS songs"
 artist_table_drop = "DROP TABLE IF EXISTS artists"
-time_table_drop = "DROP TABLE IF EXISTS time"
+time_table_drop = "DROP TABLE IF EXISTS times"
 
 # CREATE TABLES
 
 songplay_table_create = ("""
-CREATE TABLE IF NOT EXISTS songplays (songplay_id SERIAL PRIMARY KEY, start_time int, user_id varchar, level varchar, song_id varchar FOREIGN KEY, artist_id varchar FOREIGN KEY, session_id int, location varchar, user_agent varchar)
+CREATE TABLE IF NOT EXISTS songplays (songplay_id SERIAL PRIMARY KEY, start_time int, user_id varchar, level varchar, song_id varchar, artist_id varchar, session_id int, location varchar, user_agent varchar)
 """)
 
 user_table_create = ("""
@@ -36,20 +36,16 @@ INSERT INTO songplays (songplay_id, user_id, level, song_id, artist_id, session_
 
 user_table_insert = ("""
 INSERT INTO users (user_id, first_name, last_name, gender, level) VALUES (%s, %s, %s, %s, %s)
-ON CONFLICT (user_id)
-DO UPDATE SET level  = EXCLUDED.level;
+ON CONFLICT (user_id) DO UPDATE SET level  = EXCLUDED.level;
 """)
 
 song_table_insert = ("""
 INSERT INTO songs (song_id, title, artist_id, year, duration) VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT DO NOTHING;
 """)
 
 artist_table_insert = ("""
-INSERT INTO artists (artist_id, name, location, latitude, longitude) VALUES (%s, %s, %s, %s, %s)
-ON CONFLICT (artist_id)
-DO UPDATE SET location = EXCLUDED.location
-DO UPDATE SET latitude = EXCLUDED.latitude
-DO UPDATE SET longitude = EXCLUDED.longitude
+INSERT INTO artists (artist_id, name, location, latitude, longitude) VALUES (%s, %s, %s, %s, %s) ON CONFLICT DO NOTHING
 """)
 
 
