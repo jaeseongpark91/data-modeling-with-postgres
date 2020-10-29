3c7a679495d08ea966315c63450c3f9c8ae5536e
jaeseongpark <46639995+jaeseongpark91@users.noreply.github.com>
2020-10-29 16:43:42 -0400

    Update sql_queries.py
    udacity #2 revision

--- sql_queries.py
+++ sql_queries.py
@@ -9,56 +9,100 @@ time_table_drop = "DROP TABLE IF EXISTS times"
 # CREATE TABLES
 
 songplay_table_create = ("""
-CREATE TABLE IF NOT EXISTS songplays (songplay_id SERIAL PRIMARY KEY, start_time int, user_id varchar, level varchar, song_id varchar, artist_id varchar, session_id int, location varchar, user_agent varchar)
+    CREATE TABLE IF NOT EXISTS songplays (
+        songplay_id SERIAL PRIMARY KEY, 
+        start_time int, 
+        user_id int, 
+        level varchar, 
+        song_id varchar, 
+        artist_id varchar, 
+        session_id int, 
+        location varchar, 
+        user_agent varchar
+        )
 """)
 
 user_table_create = ("""
-CREATE TABLE IF NOT EXISTS users (user_id varchar PRIMARY KEY, first_name varchar, last_name varchar, gender varchar, level varchar)
+    CREATE TABLE IF NOT EXISTS users (
+        user_id int PRIMARY KEY NOT NULL, 
+        first_name varchar, 
+        last_name varchar, 
+        gender varchar, 
+        level varchar
+        )
 """)
 
 song_table_create = ("""
-CREATE TABLE IF NOT EXISTS songs (song_id varchar PRIMARY KEY, title varchar, artist_id varchar, year int, duration int)
+    CREATE TABLE IF NOT EXISTS songs (
+        song_id varchar PRIMARY KEY, 
+        title varchar, 
+        artist_id varchar, 
+        year int, 
+        duration float
+        )
 """)
 
 artist_table_create = ("""
-CREATE TABLE IF NOT EXISTS artists (artist_id varchar PRIMARY KEY, name varchar, location varchar, latitude float, longitude float)
+    CREATE TABLE IF NOT EXISTS artists (
+        artist_id varchar PRIMARY KEY, 
+        name varchar, 
+        location varchar, 
+        latitude float, 
+        longitude float
+        )
 """)
 
 time_table_create = ("""
-CREATE TABLE IF NOT EXISTS times (hour int, day int, week int, month int, year int, weekday int)
+    CREATE TABLE IF NOT EXISTS times (
+        songplay_id SERIAL PRIMARY KEY, 
+        hour int, 
+        day int, 
+        week int, 
+        month int, 
+        year int, 
+        weekday int
+        )
 """)
 
 # INSERT RECORDS
 
 songplay_table_insert = ("""
-INSERT INTO songplays (songplay_id, user_id, level, song_id, artist_id, session_id, location, user_agent) VALUES (DEFAULT, %s, %s, %s, %s, %s, %s, %s)
+    INSERT INTO songplays (songplay_id, user_id, level, song_id, artist_id, session_id, location, user_agent)
+    VALUES (DEFAULT, %s, %s, %s, %s, %s, %s, %s)
 """)
 
 user_table_insert = ("""
-INSERT INTO users (user_id, first_name, last_name, gender, level) VALUES (%s, %s, %s, %s, %s)
-ON CONFLICT (user_id) DO UPDATE SET level  = EXCLUDED.level;
+    INSERT INTO users (user_id, first_name, last_name, gender, level) 
+    VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT (user_id) DO UPDATE SET level  = EXCLUDED.level;
 """)
 
 song_table_insert = ("""
-INSERT INTO songs (song_id, title, artist_id, year, duration) VALUES (%s, %s, %s, %s, %s)
-ON CONFLICT DO NOTHING;
+    INSERT INTO songs (song_id, title, artist_id, year, duration) 
+    VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT DO NOTHING;
 """)
 
 artist_table_insert = ("""
-INSERT INTO artists (artist_id, name, location, latitude, longitude) VALUES (%s, %s, %s, %s, %s) ON CONFLICT DO NOTHING
+    INSERT INTO artists (artist_id, name, location, latitude, longitude) 
+    VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT (artist_id) DO NOTHING;
 """)
 
 
 time_table_insert = ("""
-INSERT INTO times (hour, day, week, month, year, weekday) VALUES (%s, %s, %s, %s, %s, %s)
+    INSERT INTO times (hour, day, week, month, year, weekday) 
+    VALUES (%s, %s, %s, %s, %s, %s)
 """)
 
 # FIND SONGS
 
 song_select = ("""
-SELECT songs.song_id, artists.artist_id FROM songs
-JOIN artists ON songs.artist_id = artists.artist_id
-WHERE songs.title = %s AND artists.name = %s AND songs.duration = %s;
+    SELECT songs.song_id, artists.artist_id 
+    FROM songs
+    JOIN artists ON songs.artist_id = artists.artist_id
+    WHERE songs.title = %s AND artists.name = %s AND songs.duration = %s
+    ;
 """)
 
 # QUERY LISTS
